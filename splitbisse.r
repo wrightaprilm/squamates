0ceeec72cfd2f0f24de4fa3f065b569f1094f345
April Wright <wright.aprilm@gmail.com>
2014-06-04 13:13:12 -0500

    made output export

--- splitbisse.r
+++ splitbisse.r
@@ -2,13 +2,12 @@
 
 library(diversitree)
 library(ape)
-data <- read.csv('/home/april/projectfiles/squamates/squam/PyronSmallParityData.csv', row.names=1)
+data <- read.csv('/home/april/projectfiles/squamates/squam/PyronParityData.csv', row.names=1)
 lk <- vector(mode="list")
 lkp <- vector(mode="list")
 bisse_split_fits <- vector(mode="list")
 stat_list <- vector(mode="list")
 fit_plots <- vector(mode="list")
-test_list <- vector(mode="list")
 a<- list.files(pattern="*tre")
 
 skinks <- mrca(pruned.tree)["Plestiodon_fasciatus", "Acontias_percivali"]
@@ -19,23 +18,26 @@ lacert <- mrca(pruned.tree)["Bipes_biporus", "Teius_teyou"]
 ig <- mrca(pruned.tree)["Iguana_iguana", "Chamaeleo_zeylanicus"]
 
 model_fit <- function(const,tree_list){
-for (x in tree_list){
+for (x in a){
 phy <- read.tree(x)
 pruned.tree<-drop.tip(phy, setdiff(phy$tip.label, row.names(data)))
 sorteddata <- data[phy$tip.label, ]
 no_na <- na.omit(sorteddata)
 names(no_na) <- pruned.tree$tip.label
-split_func <- make.bisse.split(pruned.tree, no_na, const)
+split_func <- make.bisse.split(pruned.tree, no_na, ig)
 func <- make.bisse(pruned.tree, no_na)
 sp <- starting.point.bisse(pruned.tree)
 pars.s <- rep(sp, 2)
 split_fit_bisse <- find.mle(split_func,pars.s)
 fit_bisse <- find.mle(func, sp)
-bisse_split_fits <- append(bisse_split_fits, split_fit_bisse)
-test <- anova(split_fit_bisse, fit_bisse)
-test_list <- append(test_list,test)
-return(test_list)
+#bisse_split_fits <- append(bisse_split_fits, split_fit_bisse)
+lapply(split_fit_bisse, write, "splitparams.txt", append=TRUE)
+lapply(fit_bisse, write, "params.txt", append=TRUE)
+sigs <- anova(split_fit_bisse, fit_bisse)
+print(x)
+lapply(sigs, write, "pref.txt", append=TRUE)
+#return(bisse_split_fits)
 }
 }
 
-model_fit(ig,a)
+
