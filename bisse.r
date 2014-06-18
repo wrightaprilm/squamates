0d6880fa13c766ba9de23ea7bbb5d41b40d5a064
April Wright <wright.aprilm@gmail.com>
2014-06-18 15:16:19 -0500

    made output clearer on bisse script

--- bisse.r
+++ bisse.r
@@ -3,11 +3,10 @@
 library(diversitree)
 library(ape)
 data <- read.csv('/home/april/projectfiles/squamates/squam/PyronSmallParityData.csv', row.names=1)
-lk <- vector(mode="list")
-lkp <- vector(mode="list")
-bisse_fits <- vector(mode="list")
-stat_list <- vector(mode="list")
-fit_plots <- vector(mode="list")
+output_vector <- vector()
+ovip_root_vector <- vector()
+vivp_root_vector <- vector()
+
 a<- list.files(pattern="*tre")
 
 model_fit <- function(tree_list){
@@ -18,20 +17,19 @@ sorteddata <- data[phy$tip.label, ]
 no_na <- na.omit(sorteddata)
 names(no_na) <- pruned.tree$tip.label
 func <- make.bisse(pruned.tree, no_na)
-lk <- append(lk, func)
 sp<-starting.point.bisse(pruned.tree)
-lkp <- append(lkp, sp)
 fit_bisse <- find.mle(func, sp)
-bisse_fits <- append(bisse_fits, fit_bisse)
 st <- asr.marginal(func, coef(fit_bisse))
-stat_list <- append(stat_list, st)
+ovip_root_vector <- append(ovip_root_vector, c(st[,2][1], 'Oviparity'))
+vivp_root_vector <- append(ovip_root_vector, c(st[,2][2], 'Viviparity'))
 outname <- paste('./bisse/', x, '.pdf', sep='')
 plot(pruned.tree, show.tip.label=FALSE)
 fit_plot <- nodelabels(pie=t(st), piecol=1:2, cex=.5)
-jpeg(outname)
-print(bisse_fits)
-dev.off()
-return(bisse_fits)
+output_vector <- append(vec, c(fit_bisse$par[1:6], fit_bisse$lnLik))
+lapply(output_vector, write, "model_params.csv", append=TRUE)
+lapply(ovip_root_vector, write, "ovip_support.csv", append=TRUE)
+lapply(vivip_root_vector, write, "vivip_support.csv", append=TRUE)
+return(c(bisse_fits,st))
 }
 }
 
