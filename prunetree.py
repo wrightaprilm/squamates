c1ed156512416d8b733010e05754f13295a4d701
ladykiyenz <klyons19@gmail.com>
2014-07-09 11:36:19 -0500

    updated prunetree.py

--- prunetree.py
+++ prunetree.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-## start with trees that have been pruned of taxa without parity mode
+## start with trees that have been pruned of taxa without parity mode (using R)
 
 import dendropy
 
@@ -52,16 +52,44 @@ lactrees = dendropy.TreeList()
 sertrees = dendropy.TreeList()
 scitrees = dendropy.TreeList()
 
-## same operations as above
+## same operations as above but for a sample of trees
 for tree in sqtrees:
-	ang_mrca = tree.mrca(taxon_labels="Varanus_indicus", "Anniella_pulchra")
-	gek_mrca = tree.mrca(taxon_labels="Phelsuma_ornata", "Delma_impar")
-	igu_mrca = tree.mrca(taxon_labels="Iguana_iguana", "Chamaeleo_zeylanicus")
-	lac_mrca = tree.mrca(taxon_labels="Bipes_biporus", "Teius_teyou")
-	ser_mrca = tree.mrca(taxon_labels="Nerodia_rhombifer", "Liotyphlops_albirostris")
-	sci_mrca = tree.mrca(taxon_labels="Plestiodon_fasciatus", "Acontias_percivali")
+	rep_node_D = tree.find_node_with_taxon_label("Sphenodon punctatus")
+	tree.reroot_at_edge(rep_node_D.edge, update_splits=True)
+	print(tree.description())
+	
+	rep_ang_mrca = tree.mrca(taxon_labels="Varanus_indicus", "Anniella_pulchra")
+	ang_tree.seed_node = rep_ang_mrca
+	angtrees.append(ang_tree)
+	tree.update_splits()	
+
+	rep_gek_mrca = tree.mrca(taxon_labels="Phelsuma_ornata", "Delma_impar")
+	gek_tree.seed_node = rep_gek_mrca
+	gektrees.append(gek_tree)
+	tree.update_splits()
+
+	rep_igu_mrca = tree.mrca(taxon_labels="Iguana_iguana", "Chamaeleo_zeylanicus")
+	igu_tree.seed_node = rep_igu_mrca
+	igutrees.append(igu_tree)
+	tree.update_splits()
+
+	rep_lac_mrca = tree.mrca(taxon_labels="Bipes_biporus", "Teius_teyou")
+	lac_tree.seed_node = rep_lac_mrca
+	lactrees.append(lac_tree)
+	tree.update_splits()
+	
+	rep_ser_mrca = tree.mrca(taxon_labels="Nerodia_rhombifer", "Liotyphlops_albirostris")
+	ser_tree.seed_node = rep_ser_mrca
+	sertrees.append(ser_tree)
+	tree.update_splits()
+
+	rep_sci_mrca = tree.mrca(taxon_labels="Plestiodon_fasciatus", "Acontias_percivali")
+	sci_tree.seed_node = rep_sci_mrca
+	scitrees.append(sci_tree)
+	tree.update_splits()
 
 
+## looking at distance with subclades from a point estimate and a sample of trees
 RF = [tree1.symmetric_difference(tree) for tree in sqtrees]
 
 
