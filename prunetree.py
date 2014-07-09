00558f357a1ded488a1f8d75f81ce600047463e3
ladykiyenz <klyons19@gmail.com>
2014-07-09 12:51:27 -0500

    fixing prunetree.py bugs

--- prunetree.py
+++ prunetree.py
@@ -3,45 +3,51 @@
 ## start with trees that have been pruned of taxa without parity mode (using R)
 
 import dendropy
+from dendropy.utility.fileutils import find_files
 
 ## operations for the ml tree
-ml = dendropy.Tree.getfrompath(filename, "newick")
+ml = dendropy.Tree.get_from_path("filename", "newick")
 print(ml.description())
 node_D = ml.find_node_with_taxon_label("Sphenodon punctatus")
 ml.reroot_at_edge(node_D.edge, update_splits=True)
-print(ml.description())
 
 ## get mrca nodes for clades
-ang_mrca = ml.mrca(taxon_labels="Varanus_indicus", "Anniella_pulchra")
-gek_mrca = ml.mrca(taxon_labels="Phelsuma_ornata", "Delma_impar")
-igu_mrca = ml.mrca(taxon_labels="Iguana_iguana", "Chamaeleo_zeylanicus")
-lac_mrca = ml.mrca(taxon_labels="Bipes_biporus", "Teius_teyou")
-ser_mrca = ml.mrca(taxon_labels="Nerodia_rhombifer", "Liotyphlops_albirostris")
-sci_mrca = ml.mrca(taxon_labels="Plestiodon_fasciatus", "Acontias_percivali")
+ang_mrca = ml.mrca(taxon_labels=["Varanus indicus", "Anniella pulchra"])
+gek_mrca = ml.mrca(taxon_labels=["Phelsuma ornata", "Delma impar"])
+igu_mrca = ml.mrca(taxon_labels=["Iguana iguana", "Chamaeleo zeylanicus"])
+lac_mrca = ml.mrca(taxon_labels=["Bipes biporus", "Teius teyou"])
+ser_mrca = ml.mrca(taxon_labels=["Nerodia rhombifer", "Liotyphlops albirostris"])
+sci_mrca = ml.mrca(taxon_labels=["Plestiodon fasciatus", "Acontias percivali"])
 
 ## pruning and writing trees goes here
+ang_ml=dendropy.Tree()
 ang_ml.seed_node = ang_mrca
-ang_ml.write.to.path("ang_ml.tre","newick")
-ml.update_splits()
+ang_ml.write_to_path("ang_ml.tre","newick")
+ml.update_splits() ##issue here, but is this command necessary?
+ang_ml=dendropy.Tree()
 gek_ml.seed_node = gek_mrca
-gek_ml.write.to.path("gek_ml.tre","newick")
+gek_ml.write_to_path("gek_ml.tre","newick")
 ml.update_splits()
+igu_ml=dendropy.Tree()
 igu_ml.seed_node = igu_mrca
-igu_ml.write.to.path("igu_ml.tre","newick")
+igu_ml.write_to_path("igu_ml.tre","newick")
 ml.update_splits()
+lac_ml=dendropy.Tree()
 lac_ml.seed_node = lac_mrca
-lac_ml.write.to.path("lac_ml.tre","newick")
+lac_ml.write_to_path("lac_ml.tre","newick")
 ml.update_splits()
+ser_ml=dendropy.Tree()
 ser_ml.seed_node = ser_mrca
-ser_ml.write.to.path("ser_ml.tre","newick")
+ser_ml.write_to_path("ser_ml.tre","newick")
 ml.update_splits()
+sci_ml=dendropy.Tree()
 sci_ml.seed_node = sci_mrca
-sci_ml.write.to.path("sci_ml.tre","newick")
+sci_ml.write_to_path("sci_ml.tre","newick")
 ml.update_splits()
 
 ## import list of trees of type newick from filepath and verify number of trees with print
-flist = find_files(top=o_file, filename_filter='*.con')
-sqtrees = [dendropy.TreeList.getfrompath(filename,"newick") for filename in flist]
+flist = find_files(top=locat, filename_filter='*.txt')
+sqtrees = [dendropy.TreeList.get_from_path(filename,"newick") for filename in flist]
 print(sqtrees.description())
 
 ## empty lists for pruned trees
@@ -90,7 +96,7 @@ for tree in sqtrees:
 
 
 ## looking at distance with subclades from a point estimate and a sample of trees
-RF = [tree1.symmetric_difference(tree) for tree in sqtrees]
+RF = [ml.symmetric_difference(tree) for tree in sqtrees]
 
 
 
