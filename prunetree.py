c354ff28593de0b3e9301c214bed09b609e7cff8
ladykiyenz <klyons19@gmail.com>
2014-07-15 02:11:09 -0500

    fixed prunetree.py, added rfdist.py

--- prunetree.py
+++ prunetree.py
@@ -1,7 +1,5 @@
 #! /usr/bin/env python
 
-## start with trees that have been pruned of taxa without parity mode (using R)
-
 import dendropy
 from dendropy.utility.fileutils import find_files
 
@@ -23,32 +21,32 @@ sci_mrca = ml.mrca(taxon_labels=["Plestiodon fasciatus", "Acontias percivali"])
 ang_ml=dendropy.Tree()
 ang_ml.seed_node = ang_mrca
 ang_ml.write_to_path("ang_ml.tre","newick")
-ml.update_splits() ##issue here, but is this command necessary?
-ang_ml=dendropy.Tree()
+
+gek_ml=dendropy.Tree()
 gek_ml.seed_node = gek_mrca
 gek_ml.write_to_path("gek_ml.tre","newick")
-ml.update_splits()
+
 igu_ml=dendropy.Tree()
 igu_ml.seed_node = igu_mrca
 igu_ml.write_to_path("igu_ml.tre","newick")
-ml.update_splits()
+
 lac_ml=dendropy.Tree()
 lac_ml.seed_node = lac_mrca
 lac_ml.write_to_path("lac_ml.tre","newick")
-ml.update_splits()
+
 ser_ml=dendropy.Tree()
 ser_ml.seed_node = ser_mrca
 ser_ml.write_to_path("ser_ml.tre","newick")
-ml.update_splits()
+
 sci_ml=dendropy.Tree()
 sci_ml.seed_node = sci_mrca
 sci_ml.write_to_path("sci_ml.tre","newick")
-ml.update_splits()
 
-## import list of trees of type newick from filepath and verify number of trees with print
-flist = find_files(top=locat, filename_filter='*.txt')
-sqtrees = [dendropy.TreeList.get_from_path(filename,"newick") for filename in flist]
-print(sqtrees.description())
+
+## uncomment if trees are in separate files, import list of trees of type newick
+##flist = find_files(top='trees', filename_filter='*.dated.tre')
+##sqtrees = [dendropy.TreeList.get_from_path(filename,"newick") for filename in flist]
+##print(sqtrees.description(2))
 
 ## empty lists for pruned trees
 angtrees = dendropy.TreeList()
@@ -62,41 +60,55 @@ scitrees = dendropy.TreeList()
 for tree in sqtrees:
 	rep_node_D = tree.find_node_with_taxon_label("Sphenodon punctatus")
 	tree.reroot_at_edge(rep_node_D.edge, update_splits=True)
-	print(tree.description())
-	
-	rep_ang_mrca = tree.mrca(taxon_labels="Varanus_indicus", "Anniella_pulchra")
+	rep_ang_mrca = tree.mrca(taxon_labels=["Varanus indicus", "Anniella pulchra"])
+	ang_tree=dendropy.Tree()
 	ang_tree.seed_node = rep_ang_mrca
 	angtrees.append(ang_tree)
 	tree.update_splits()	
-
-	rep_gek_mrca = tree.mrca(taxon_labels="Phelsuma_ornata", "Delma_impar")
+	rep_gek_mrca = tree.mrca(taxon_labels=["Phelsuma ornata", "Delma impar"])
+	gek_tree=dendropy.Tree()	
 	gek_tree.seed_node = rep_gek_mrca
 	gektrees.append(gek_tree)
 	tree.update_splits()
-
-	rep_igu_mrca = tree.mrca(taxon_labels="Iguana_iguana", "Chamaeleo_zeylanicus")
+	rep_igu_mrca = tree.mrca(taxon_labels=["Iguana iguana", "Chamaeleo zeylanicus"])
+	igu_tree=dendropy.Tree()
 	igu_tree.seed_node = rep_igu_mrca
 	igutrees.append(igu_tree)
 	tree.update_splits()
-
-	rep_lac_mrca = tree.mrca(taxon_labels="Bipes_biporus", "Teius_teyou")
+	rep_lac_mrca = tree.mrca(taxon_labels=["Bipes biporus", "Teius teyou"])
+	lac_tree=dendropy.Tree()
 	lac_tree.seed_node = rep_lac_mrca
 	lactrees.append(lac_tree)
 	tree.update_splits()
-	
-	rep_ser_mrca = tree.mrca(taxon_labels="Nerodia_rhombifer", "Liotyphlops_albirostris")
+	rep_ser_mrca = tree.mrca(taxon_labels=["Nerodia rhombifer", "Liotyphlops albirostris"])
+	ser_tree=dendropy.Tree()	
 	ser_tree.seed_node = rep_ser_mrca
 	sertrees.append(ser_tree)
 	tree.update_splits()
-
-	rep_sci_mrca = tree.mrca(taxon_labels="Plestiodon_fasciatus", "Acontias_percivali")
+	rep_sci_mrca = tree.mrca(taxon_labels=["Plestiodon fasciatus", "Acontias percivali"])
+	sci_tree=dendropy.Tree()
 	sci_tree.seed_node = rep_sci_mrca
 	scitrees.append(sci_tree)
 	tree.update_splits()
 
+## empty lists for RF distance by subclade
+allrf = []
+angrf = []
+gekrf = []
+igurf = []
+lacrf = []
+serrf = []
+scirf = []
+
 
 ## looking at distance with subclades from a point estimate and a sample of trees
-RF = [ml.symmetric_difference(tree) for tree in sqtrees]
 
+allrf.append[ml.symmetric_difference(tree) for tree in sqtrees]
+angrf.append[ang_ml.symmetric_difference(tree) for tree in angtrees]
+gekrf.append[gek_ml.symmetric_difference(tree) for tree in gektrees]
+igurf.append[igu_ml.symmetric_difference(tree) for tree in igutrees]
+lacrf.append[lac_ml.symmetric_difference(tree) for tree in lactrees]
+serrf.append[ser_ml.symmetric_difference(tree) for tree in sertrees]
+scirf.append[sci_ml.symmetric_difference(tree) for tree in scitrees]
 
 
