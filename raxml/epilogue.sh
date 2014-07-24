75d5af5ffa9e216ee531d90b7bc59cb1310f7cfe
April Wright <wright.aprilm@gmail.com>
2014-07-24 13:36:21 -0500

    actually adding trees

--- raxml/epilogue.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-    date +'%s %a %b %e %R:%S %Z %Y' > /projects/ps-ngbt/backend/trestles_workspace/NGBW-JOB-RAXMLHPC2_TGB-FC2C66E7433F4722B05A18A9DD81A132/term.txt
-    echo "ExitCode=${10}" >> /projects/ps-ngbt/backend/trestles_workspace/NGBW-JOB-RAXMLHPC2_TGB-FC2C66E7433F4722B05A18A9DD81A132/term.txt
-    echo -e "Job Id: $1\nResource List: $6\nResources Used: $7\nQueue Name: $8\n" >> /projects/ps-ngbt/backend/trestles_workspace/NGBW-JOB-RAXMLHPC2_TGB-FC2C66E7433F4722B05A18A9DD81A132/term.txt
